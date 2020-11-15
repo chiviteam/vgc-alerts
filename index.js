ee7051a280cd0553c230b80175ea3e739fabd78b
gvdenbro <gregory.vandenbroucke@gmail.com>
2020-11-15 15:52:20 +0100

    first commit of vgc-alerts

---
+++ index.js
@@ -0,0 +1,51 @@
+//@ts-check
+const {firefox} = require('playwright');
+const fs = require('fs').promises;
+var TurndownService = require('turndown');
+
+async function grab(turndownService, context, page, ageIoan, center) {
+
+    await fs.rm(center.name, {recursive: true, force: true})
+
+    await page.goto(`https://tickets.vgc.be/activity/index?&vrijeplaatsen=1&Age%5B%5D=${ageIoan}%2C${ageIoan + 1}&entity=${center.id}`);
+
+    // this little trick makes relative links absolute -> this way we get nice absolute urls in generated files
+    await page.$$eval('.transactivity a', (links) => {
+        links.forEach(link => {
+            link.href = link.href
+        })
+    });
+
+    const [activities] = await Promise.all([page.$$('.transactivity')]);
+
+    for (const element of activities) {
+        let dataId = await element.getAttribute("data-id");
+        await element.screenshot({path: `${center.name}/${dataId}.png`});
+        let innerHtml = await element.innerHTML();
+        //await fs.writeFile(`${center.name}/${dataId}.html`, `<div>${innerHtml}</div>`);
+        await fs.writeFile(`${center.name}/${dataId}.md`, turndownService.turndown(`<div>${innerHtml}</div>`));
+    }
+
+    await context.clearCookies();
+}
+
+(async () => {
+
+    console.log("Starting...")
+
+    const ageIoan = 3;
+    const centra = [{name: 'essegem', id: 109}, {name: 'demarkten', id: 244}, {name: 'nekkersdal', id: 241}]
+
+    var turndownService = new TurndownService({emDelimiter: '*'}).remove('script');
+
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    for (const center of centra) {
+        await grab(turndownService, context, page, ageIoan, center);
+    }
+
+    await browser.close();
+    console.log("Finished.");
+})();
